949bcc68540057247d065a168b9d4ddf960088a7
Xining Wang <tamuwin2016@tamu.edu>
2017-04-21 09:18:29 -0500

    try closeFacility service

--- app/assets/javascripts/trip_planner.js
+++ app/assets/javascripts/trip_planner.js
@@ -57,7 +57,8 @@ $(document).ready(function(){
         esriConfig.defaults.io.corsDetection = false;
         urlUtils.addProxyRule({
           urlPrefix: "route.arcgis.com",
-          proxyUrl: "http://utility.arcgis.com/usrsvcs/appservices/CY6AgGvTN8MqThaM/rest/services/World/Route/NAServer/Route_World"
+          //proxyUrl: "http://utility.arcgis.com/usrsvcs/appservices/CY6AgGvTN8MqThaM/rest/services/World/Route/NAServer/Route_World"
+          proxyUrl: "http://utility.arcgis.com/usrsvcs/appservices/98jbwwI2IH27ambo/rest/services/World/ClosestFacility/GPServer/FindClosestFacilities"
         });
 
 
